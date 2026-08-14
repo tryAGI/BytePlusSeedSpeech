e9cef827e24ac5f4dc00aa60873eea2a4ae6eeab
Konstantin S. <havendv@gmail.com>
2026-08-14 19:56:08 +0400

    feat: Updated OpenAPI spec (#2)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/BytePlusSeedSpeech/Generated/BytePlusSeedSpeech.BytePlusSeedSpeechClient.g.cs
+++ src/libs/BytePlusSeedSpeech/Generated/BytePlusSeedSpeech.BytePlusSeedSpeechClient.g.cs
@@ -34,10 +34,17 @@ namespace BytePlusSeedSpeech
 
         /// <inheritdoc/>
         public global::BytePlusSeedSpeech.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::BytePlusSeedSpeech.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::BytePlusSeedSpeech.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
